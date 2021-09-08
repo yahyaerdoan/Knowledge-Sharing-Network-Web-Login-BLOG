441116a515448539eaf03ff3706aef8004e441c7
yahyaerdoan <77584301+yahyaerdoan@users.noreply.github.com>
2021-09-08 23:27:27 +0300

    Entity Classları oluşturuldu ve özellikleri yazıldı.

---
+++ EntityLayer/Concrete/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace EntityLayer.Concrete
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public string CategoryDescription { get; set; }
+        public bool CategoryStatus { get; set; }
+    }
+}
