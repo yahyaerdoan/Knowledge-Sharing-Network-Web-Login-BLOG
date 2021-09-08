e1dab906551112a87c248c2af3dbf12a3f4c92da
yahyaerdoan <77584301+yahyaerdoan@users.noreply.github.com>
2021-09-08 22:21:09 +0300

    Add project files.

---
+++ Knowledge-Sharing-Network-weB-LOGin-BLOG/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Knowledge_Sharing_Network_weB_LOGin_BLOG.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
