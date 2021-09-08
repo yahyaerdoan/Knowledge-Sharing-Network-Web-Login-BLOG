5fbd74f89df50a0549dc6010c7fc6b3a5d0836b2
yahyaerdoan <77584301+yahyaerdoan@users.noreply.github.com>
2021-09-08 23:10:08 +0300

    Test ve Deneme çalışması yapıldı.

--- Knowledge-Sharing-Network-weB-LOGin-BLOG/Controllers/HomeController.cs
+++ Knowledge-Sharing-Network-weB-LOGin-BLOG/Controllers/HomeController.cs
@@ -33,5 +33,10 @@ namespace Knowledge_Sharing_Network_weB_LOGin_BLOG.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Test()
+        {
+            return View();
+        }
     }
 }
