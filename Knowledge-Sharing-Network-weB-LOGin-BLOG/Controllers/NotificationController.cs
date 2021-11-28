d4076e30715d047e34ddee34828f0db112a55dc4
yahyaerdoan <77584301+yahyaerdoan@users.noreply.github.com>
2021-11-28 16:19:56 +0300

    Bildirimler Sınıfı Nesnleri Kendi Sayfasında Listelendi.

--- Knowledge-Sharing-Network-weB-LOGin-BLOG/Controllers/NotificationController.cs
+++ Knowledge-Sharing-Network-weB-LOGin-BLOG/Controllers/NotificationController.cs
@@ -3,14 +3,24 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Knowledge_Sharing_Network_weB_LOGin_BLOG.Controllers
 {
     public class NotificationController : Controller
     {
+        NotificationManager notificationManager = new NotificationManager(new EfNotificationDal());
         public IActionResult Index()
         {
             return View();
+        }
+        [AllowAnonymous]
+        public IActionResult AllNotification()
+        {
+            var values = notificationManager.GetAll();
+            return View(values);
         }
     }
 }
