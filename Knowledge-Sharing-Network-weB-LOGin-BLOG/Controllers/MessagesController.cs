b388695bc585cd015da903f1f8e3bb92af02a302
yahyaerdoan <77584301+yahyaerdoan@users.noreply.github.com>
2021-11-29 16:48:35 +0300

    Mesaj Gelen Kutusu Listelendi ve Detaylarının Görüntülenmesi Sağlandı.

---
+++ Knowledge-Sharing-Network-weB-LOGin-BLOG/Controllers/MessagesController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Knowledge_Sharing_Network_weB_LOGin_BLOG.Controllers
+{
+    public class MessagesController : Controller
+    {
+        MessageManager messageManager = new MessageManager(new EfMessageDal());
+        [AllowAnonymous]
+        public IActionResult InBox()
+        {
+            int id = 1;
+            var values = messageManager.GetInBoxMessageListByWriterId(id);
+            return View(values);
+        }
+        [AllowAnonymous]
+        public IActionResult MessageDetails(int id)
+        {
+            var values = messageManager.GetById(id);
+            return View(values);
+        }
+    }
+}
