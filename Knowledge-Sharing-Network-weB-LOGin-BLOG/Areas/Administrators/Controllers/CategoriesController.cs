0f81d25f70baecf36401c9dce493f969ccef46ae
yahyaerdoan <77584301+yahyaerdoan@users.noreply.github.com>
2021-11-30 21:52:29 +0300

    X.PagedList ve X.PagedList.Mvc.Core Paketleri Administrators Yönetiminde Sayfalama (pagination) Yapmak İçin Eklendi. Categories Listelendi ve Sayfalandı.

--- Knowledge-Sharing-Network-weB-LOGin-BLOG/Areas/Administrators/Controllers/CategoriesController.cs
+++ Knowledge-Sharing-Network-weB-LOGin-BLOG/Areas/Administrators/Controllers/CategoriesController.cs
@@ -1,13 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using X.PagedList;
+using X.PagedList.Mvc.Core;
 
 namespace Knowledge_Sharing_Network_weB_LOGin_BLOG.Areas.Administrators.Controllers
 {
     [Area("Administrators")]
     public class CategoriesController : Controller
     {
-        public IActionResult Index()
+        readonly CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+
+        [Route("Administrators/[Controller]/[Action]/{page?}")]
+        public IActionResult Index(int page = 1)
         {
-            return View();
+            return View(categoryManager.GetAll().ToPagedList(page, 6));
         }
     }
 }
