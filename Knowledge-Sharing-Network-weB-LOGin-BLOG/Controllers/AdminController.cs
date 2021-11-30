c147a522abdf9652e9cb00996b41db7919f07366
yahyaerdoan <77584301+yahyaerdoan@users.noreply.github.com>
2021-11-30 13:50:53 +0300

    Admin Layout Oluşturuldu. Adnin Navbar Nesneleri Partiala Taşındı ve Yeniden Düzenlendi.

---
+++ Knowledge-Sharing-Network-weB-LOGin-BLOG/Controllers/AdminController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Knowledge_Sharing_Network_weB_LOGin_BLOG.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+       
+        public PartialViewResult PartialAdminNavbar()
+        {
+            return PartialView();
+        }
+    }
+}
