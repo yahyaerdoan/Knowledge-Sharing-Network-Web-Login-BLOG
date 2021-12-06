e9c0787d4e24c57675ffddd7c50951b7f8ea895e
yahyaerdoan <77584301+yahyaerdoan@users.noreply.github.com>
2021-12-07 00:36:12 +0300

    Widgets Sayfası Oluşturuldu. Sayfada Düzenlemeler Yapıldı. Administrator Footer Yeniden Oluşturuldu.

---
+++ Knowledge-Sharing-Network-weB-LOGin-BLOG/Areas/Administrator/Controllers/WidgetsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Knowledge_Sharing_Network_weB_LOGin_BLOG.Areas.Administrator.Controllers
+{
+    [Area("Administrator")]
+    public class WidgetsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
