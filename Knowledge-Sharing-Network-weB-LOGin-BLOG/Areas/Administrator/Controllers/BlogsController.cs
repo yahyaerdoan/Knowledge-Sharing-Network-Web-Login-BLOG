2f558f57375f979b96b6721f4ad038f93501bb61
yahyaerdoan <77584301+yahyaerdoan@users.noreply.github.com>
2021-12-06 23:38:17 +0300

    Export Excel İşlemi Dynamic Olarak Listelendi.

--- Knowledge-Sharing-Network-weB-LOGin-BLOG/Areas/Administrator/Controllers/BlogsController.cs
+++ Knowledge-Sharing-Network-weB-LOGin-BLOG/Areas/Administrator/Controllers/BlogsController.cs
@@ -6,6 +6,8 @@ using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using ClosedXML.Excel;
+using DataAccessLayer.Concrete.Context;
+using EntityLayer.Concrete;
 using Knowledge_Sharing_Network_weB_LOGin_BLOG.Areas.Administrator.Models;
 
 namespace Knowledge_Sharing_Network_weB_LOGin_BLOG.Areas.Administrator.Controllers
@@ -51,8 +53,62 @@ namespace Knowledge_Sharing_Network_weB_LOGin_BLOG.Areas.Administrator.Controlle
             return blogModels;
         }
 
-        
+
         public IActionResult BlogsListExcel()
+        {
+            return View();
+        }
+
+        public IActionResult ExportDynamicExcelBlogsList()
+        {
+            using (var workbook = new XLWorkbook())
+            {
+                var worksheet = workbook.Worksheets.Add("Bloglar Listesi");
+                worksheet.Cell(1, 1).Value = "Blog Id";
+                worksheet.Cell(1, 2).Value = "Blog Adı";
+                worksheet.Cell(1, 3).Value = "Blog İçeriği";
+                worksheet.Cell(1, 4).Value = "Blog Tarih";
+                worksheet.Cell(1, 5).Value = "Blog Durum";
+
+                int BlogRowCount = 2;
+                foreach (var item in BlogsTitleList())
+                {
+                    worksheet.Cell(BlogRowCount, 1).Value = item.BlogId;
+                    worksheet.Cell(BlogRowCount, 2).Value = item.BlogTitle;
+                    worksheet.Cell(BlogRowCount, 3).Value = item.BlogContent;
+                    worksheet.Cell(BlogRowCount, 4).Value = item.BlogCreateDate;
+                    worksheet.Cell(BlogRowCount, 5).Value = item.BlogStatus;
+                    BlogRowCount++;
+                }
+
+                using (var stream = new MemoryStream())
+                {
+                    workbook.SaveAs(stream);
+                    var content = stream.ToArray();
+                    return File(content, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "Bloglar Listesi.xlsx");
+                }
+            }
+        }
+
+        public List<BlogModel> BlogsTitleList()
+        {
+            List<BlogModel> blogsModel = new List<BlogModel>();
+            using (var context = new WebLogContext())
+            {
+                blogsModel = context.Blogs.Select(x => new BlogModel()
+                {
+                    BlogId = x.BlogId,
+                    BlogTitle = x.BlogTitle,
+                    BlogContent = x.BlogContent,
+                    BlogCreateDate = x.BlogCreateDate,
+                    BlogStatus = x.BlogStatus
+
+                }).ToList();
+            }
+            return blogsModel;
+        }
+
+        public IActionResult BlogsTitleListExcel()
         {
             return View();
         }
--- Knowledge-Sharing-Network-weB-LOGin-BLOG/Areas/Administrator/Models/BlogModel.cs
+++ Knowledge-Sharing-Network-weB-LOGin-BLOG/Areas/Administrator/Models/BlogModel.cs
@@ -9,5 +9,8 @@ namespace Knowledge_Sharing_Network_weB_LOGin_BLOG.Areas.Administrator.Models
     {
         public int BlogId { get; set; }
         public string BlogTitle { get; set; }
+        public string BlogContent { get; set; }
+        public DateTime BlogCreateDate { get; set; }
+        public bool BlogStatus { get; set; }
     }
 }
