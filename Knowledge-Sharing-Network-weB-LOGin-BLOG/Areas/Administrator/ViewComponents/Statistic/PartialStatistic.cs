54fd644043e59d63981802a2c017b09571289f71
yahyaerdoan <77584301+yahyaerdoan@users.noreply.github.com>
2021-12-14 21:05:51 +0300

    İstatistik İçin Partial Oluşturuldu. Toplam Blog, Yorum ve Yeni Mesaj Sayıları Gösterildi.

---
+++ Knowledge-Sharing-Network-weB-LOGin-BLOG/Areas/Administrator/ViewComponents/Statistic/PartialStatistic.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete.Context;
+using DataAccessLayer.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Knowledge_Sharing_Network_weB_LOGin_BLOG.Areas.Administrator.ViewComponents.Statistic
+{
+    public class PartialStatistic : ViewComponent
+    {
+        private readonly BlogManager blogManager = new BlogManager(new EfBlogDal());
+        private readonly WebLogContext webLogContext = new WebLogContext();
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.blogTotalCount = blogManager.GetAll().Count();
+            ViewBag.newMessageCount = webLogContext.Contacts.Count();
+            ViewBag.commetTotalCount = webLogContext.Comments.Count();
+            return View();
+        }
+    }
+}
