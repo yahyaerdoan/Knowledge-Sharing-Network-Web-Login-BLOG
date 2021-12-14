15e2206e3ba5d3447a3930a019552377becbfd16
yahyaerdoan <77584301+yahyaerdoan@users.noreply.github.com>
2021-12-15 01:07:53 +0300

    İstatistik İsimleri Yeniden Düzenlendi. Metinsel İstatistikler Yapı Oluşturuldu ve Yayınlandı.

---
+++ Knowledge-Sharing-Network-weB-LOGin-BLOG/Areas/Administrator/ViewComponents/Statistic/PartialStatisticScalar.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BusinessLayer.Abstract;
+using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete.Context;
+using DataAccessLayer.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Knowledge_Sharing_Network_weB_LOGin_BLOG.Areas.Administrator.ViewComponents.Statistic
+{
+    public class PartialStatisticScalar : ViewComponent
+    {
+        private readonly IBlogService _blogService;
+
+        //private readonly BlogManager blogManager = new BlogManager(new EfBlogDal());
+        private readonly WebLogContext webLogContext = new WebLogContext();
+
+        public PartialStatisticScalar(IBlogService blogService)
+        {
+            _blogService = blogService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.blogTotalCount = _blogService.GetByCount();
+            ViewBag.newMessageCount = webLogContext.Contacts.Count();
+            ViewBag.commetTotalCount = webLogContext.Comments.Count();
+            return View();
+        }
+    }
+}
--- Knowledge-Sharing-Network-weB-LOGin-BLOG/Areas/Administrator/ViewComponents/Statistic/PartialStatisticTextual.cs
+++ Knowledge-Sharing-Network-weB-LOGin-BLOG/Areas/Administrator/ViewComponents/Statistic/PartialStatisticTextual.cs
@@ -3,28 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using BusinessLayer.Abstract;
-using BusinessLayer.Concrete;
 using DataAccessLayer.Concrete.Context;
-using DataAccessLayer.Concrete.EntityFramework;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Knowledge_Sharing_Network_weB_LOGin_BLOG.Areas.Administrator.ViewComponents.Statistic
 {
-    public class PartialStatistic : ViewComponent
+    public class PartialStatisticTextual : ViewComponent
     {
         private readonly IBlogService _blogService;
 
-        //private readonly BlogManager blogManager = new BlogManager(new EfBlogDal());
         private readonly WebLogContext webLogContext = new WebLogContext();
 
-        public PartialStatistic(IBlogService blogService)
+        public PartialStatisticTextual(IBlogService blogService)
         {
             _blogService = blogService;
         }
-
         public IViewComponentResult Invoke()
         {
-            ViewBag.blogTotalCount = _blogService.GetByCount();
+            ViewBag.blogTotalCount = _blogService.GetAll()
+                .OrderByDescending(x => x.BlogId)
+                .Select(b => b.BlogTitle)
+                .Take(1).FirstOrDefault();
             ViewBag.newMessageCount = webLogContext.Contacts.Count();
             ViewBag.commetTotalCount = webLogContext.Comments.Count();
             return View();
