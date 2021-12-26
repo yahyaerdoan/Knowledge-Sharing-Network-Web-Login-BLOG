5da28ee98c6aef51b041effe20f92c816c2b6c33
yahyaerdoan <77584301+yahyaerdoan@users.noreply.github.com>
2021-12-27 00:20:27 +0300

    Google Charts Kullanılarak Statik  3D Pie & PieChart Oluşturuldu.

---
+++ Knowledge-Sharing-Network-weB-LOGin-BLOG/Areas/Administrator/Controllers/ChartsController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Knowledge_Sharing_Network_weB_LOGin_BLOG.Areas.Administrator.Models;
+
+namespace Knowledge_Sharing_Network_weB_LOGin_BLOG.Areas.Administrator.Controllers
+{
+    [Area("Administrator")]
+    public class ChartsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CategoryChart()
+        {
+            // ReSharper disable once UseObjectOrCollectionInitializer
+            List<CategoryModel> categoryModels = new List<CategoryModel>();
+            categoryModels.Add(new CategoryModel
+            {
+                categoryname = "Teknoloji",
+                categorycount = 23
+
+            });
+            categoryModels.Add(new CategoryModel
+            {
+                categoryname = "Kimya",
+                categorycount = 26
+
+            });
+            categoryModels.Add(new CategoryModel
+            {
+                categoryname = "Tatlı",
+                categorycount = 28
+
+            });
+            categoryModels.Add(new CategoryModel
+            {
+                categoryname = "İçecek",
+                categorycount = 22
+
+            });
+            categoryModels.Add(new CategoryModel
+            {
+                categoryname = "Oyuncak",
+                categorycount = 14
+
+            });
+            return Json(new {jsoncategoryModels = categoryModels});
+        }
+    }
+}
---
+++ Knowledge-Sharing-Network-weB-LOGin-BLOG/Areas/Administrator/Models/CategoryModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Knowledge_Sharing_Network_weB_LOGin_BLOG.Areas.Administrator.Models
+{
+    public class CategoryModel
+    {
+        public int categorycount { get; set; }
+        public string categoryname { get; set; }
+    }
+}
