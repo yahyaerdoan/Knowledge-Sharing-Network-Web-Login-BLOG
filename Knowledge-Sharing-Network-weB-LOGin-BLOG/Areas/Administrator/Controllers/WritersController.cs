90bfb44572ceddc25f4e195896c2ad914bab4ff8
yahyaerdoan <77584301+yahyaerdoan@users.noreply.github.com>
2022-01-12 17:54:54 +0300

    Yazarlar Ajax ile Consoleda Listelendi.

---
+++ Knowledge-Sharing-Network-weB-LOGin-BLOG/Areas/Administrator/Controllers/WritersController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Knowledge_Sharing_Network_weB_LOGin_BLOG.Areas.Administrator.Models;
+using Newtonsoft.Json;
+
+namespace Knowledge_Sharing_Network_weB_LOGin_BLOG.Areas.Administrator.Controllers
+{
+    [Area("Administrator")]
+    public class WritersController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult WriterList()
+        {
+            var jsonWriters = JsonConvert.SerializeObject(writerModels);
+            return Json(jsonWriters);
+        }
+
+        public static readonly List<WriterModel> writerModels = new List<WriterModel>
+        {
+            new WriterModel
+            {
+                Id = 1,
+                Name = "Yahya"
+            },
+            new WriterModel
+            {
+                Id = 2,
+                Name = "Kerem"
+            },
+            new WriterModel
+            {
+                Id = 3,
+                Name = "Ayşe"
+            }
+        };
+    }
+}
---
+++ Knowledge-Sharing-Network-weB-LOGin-BLOG/Areas/Administrator/Models/WriterModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Knowledge_Sharing_Network_weB_LOGin_BLOG.Areas.Administrator.Models
+{
+    public class WriterModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
